e09adec107e675b685943126d7115910d1888d7a
github_Josephkagimu1 <kagimujoseph00@gmail.com>
2021-11-01 15:39:42 -0700

    Todo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h2>Welcome to React</h2>
+        <h1>Todo List</h1>
       </header>
+
+      <Form inputText={inputText} todos={todos} setTodos={setTodos} setinputText={setInputText} />
+      <TodoList todos={todos} />
     </div>
   );
 }
